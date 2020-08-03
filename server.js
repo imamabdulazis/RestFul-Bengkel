01bd31801eda0dd9494652e07a3955a52e14e796
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-08-03 20:26:50 +0700

    update apps

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, function () {
+server.listen(0.0.0.0 || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, server.env);
 });
