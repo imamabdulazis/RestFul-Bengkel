1d31fa1043fb8041b926b0d67c78806ec65e92b6
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-08-03 20:45:03 +0700

    udate

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("listen port 3000")
+});
 
 // const server = http.createServer(app);
 
 // server.listen(port, () => {
 //     console.log("APP SART 3000");
 // });
-
-app.listen(port, () => {
-    console.log("listen port 3000")
-})
