45d71508743f88ec86309483207c3e06490ee32e
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-08-03 20:51:46 +0700

    update apps

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const app = require('./app');
+// const app = require('./app');
+const express = require("express");
+const app = express();
 
 let port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send("test")
+})
 
 app.listen(port, () => {
     console.log("listen port 3000")
