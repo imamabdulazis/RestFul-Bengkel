3042ffdecdb7a16c9d0c3fcb948fdd6647057fa1
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-08-03 20:21:07 +0700

    update apps

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
-
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log("APP SART 3000");
+server.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, server.env);
 });
